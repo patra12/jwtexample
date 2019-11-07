6968155c11e338344e8765561eac1afb5370e795
birja <birja@kusmail.com>
2019-11-08 00:35:29 +0530

    adding start

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const bodyParser = require('body-parser')
+const app = express()
+
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = process.env.NODE_ENV !== 'production'
+
+const models = require('./models/index.js')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/api/users', (req, res) => {
+  //console.log(req.body)
+  //return req.body
+  models.User.create({ email: req.body.email, password: req.body.password })
+})
+
+app.get('/api/userlist', (req, res) => {
+  //  models.User.create({ email: req.body.email, password: req.body.password})
+})
+
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+  const { host, port } = nuxt.options.server
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  } else {
+    await nuxt.ready()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on https://${host}:${port}`,
+    badge: true
+  })
+}
+start()
