750c16ac8edcb5b32dea43dccc991646d58650b6
birja <birja@kusmail.com>
2019-11-12 22:56:10 +0530

    db credential

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
     connectionLimit: 100, //important
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: "node_auth",
+    host: 'mysql://b127e5894c974c:fe2c7d90@us-cdbr-iron-east-05.cleardb.net/heroku_733928f9843c014?reconnect=true',
+    user: 'b127e5894c974c',
+    password: 'fe2c7d90',
+    database: "heroku_733928f9843c014",
     debug: false
 });
 module.exports = pool;
