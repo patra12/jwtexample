48e1e596a2a1a74b5a6319e98e1b29c53f045058
birja <birja@kusmail.com>
2019-11-25 19:44:13 +0530

    title issue fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,12 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: '%s - ' + "Dimond Tools"|| process.env.npm_package_name,
     title: 'Avona',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || 'Diamond tools' }
+      { hid: 'description', name: 'description', content: "Avona - diamond tools" || process.env.npm_package_description  }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
